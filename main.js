983385577859496fca145ea1b72d0d4c9a7ba0ba
joaodk3 <cadavsales@gmail.com>
2022-02-23 15:35:29 -0600

    Added interactivity to projects

--- main.js
+++ main.js
@@ -6,33 +6,38 @@ let image2 = document.getElementById('image2');
 let item3 = document.getElementById('ada-programming-language');
 let image3 = document.getElementById('image3');
 
-// Function that opens up the first image
-let showImage1 = function () {
-    image1.style.display = "block";
-    image1.style.visibility = "visible";
-}
-item1.onclick = showImage1;
 
-// Function that closes the image
-let closeImage1 = function () {
-    image1.style.display = "none";
-    image1.style.visibility = "hidden";
-}
-item1.onclick = closeImage1;
+// Functions to open and close the projects' images
 
-// Function that opens up the second image
-let showImage2 = function () {
-    image2.style.display = "block";
-    image2.style.visibility ="visible";
-}
-item2.onclick = showImage2;
+let openCloseImage1 = function () {
+    if (image1.style.display == "none") {
+        image1.style.display = "block";
+    } else {
+        image1.style.display = "none"
+    }
+};
 
-// Function that opens up the third image
-let showImage3 = function () {
-    image3.style.display = "block";
-    image3.style.visibility = "visible";
-}
-item3.onclick = showImage3;
+
+let openCloseImage2 = function () {
+    if (image2.style.display == "none") {
+        image2.style.display = "block";
+    } else {
+        image2.style.display = "none"
+    }
+};
+
+
+let openCloseImage3 = function () {
+    if (image3.style.display == "none") {
+        image3.style.display = "block";
+    } else {
+        image3.style.display = "none"
+    }
+};
+
+item1.onclick = openCloseImage1;
+item2.onclick = openCloseImage2;
+item3.onclick = openCloseImage3;
 
 
 // Time calculation
---
+++ readme.mk
@@ -0,0 +1,5 @@
+This is a Portfolio Project of Ada Lovelace known for being first person to right an algorithm to be processed by a machine, Charles Babbage's Analytical Engine.
+
+In this project I have used HTML, CSS and JavaScript.
+
+I also added Icons from Font Awesome Website and Font from Google Fonts
