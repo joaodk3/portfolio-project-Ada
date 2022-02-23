7bf400d4aafa1611b6b3927d342d00e1f7ff833f
joaodk3 <cadavsales@gmail.com>
2022-02-22 20:09:46 -0600

    Added HTML and style

---
+++ main.js
@@ -0,0 +1,15 @@
+// Declaring variables
+let firstComputerProgram = document.getElementById("first-computer-program");
+let analyticalEngine = document.getElementById("analytical-engine");
+let adaProgrammingLanguage = document.getElementById("ada-programming-language");
+
+let image1 = document.getElementById("image1");
+let image2 = document.getElementById("image2");
+let image2 = document.getElementById("image2");
+
+// Appear image 1 
+let appearImage1 = function () {
+    image1.innerHTML="oi";
+}
+firstComputerProgram.onclick = appearImage1;
+
